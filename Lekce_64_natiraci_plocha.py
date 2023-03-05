669d48abbe2e01f8de2ecdbe934e131d5147a498
Veronika Ferencikova <V.Strbikova@seznam.cz>
2023-03-05 10:13:10 +0100

    Vypočet plochy zdi

--- Lekce_64_natiraci_plocha.py
+++ Lekce_64_natiraci_plocha.py
@@ -1,4 +1,9 @@
-
+# načteme hodnoty od uživatele
 vyska_zdi = input("Zadej výšku zdi v metrech:\n")
 sirka_zdi = input("Zadej šířku zdi v metrech:\n")
+pokryti = 5
+
+# vypočet plochy zdi
+vypocet_plochy = int(vyska_zdi) * int(sirka_zdi)
+print(vypocet_plochy)
 
